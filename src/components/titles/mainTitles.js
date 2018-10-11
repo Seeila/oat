0f626965b6713efc7bebd9921d896cfc85d211bf
Sarah Hick <sarah0032@hotmail.com>
2018-10-11 15:06:00 +0200

    Porptypes testing added

--- src/components/titles/mainTitles.js
+++ src/components/titles/mainTitles.js
@@ -1,5 +1,10 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 const Nav = ({ titleText }) => <h1 className="main-title">{titleText}</h1>;
+
+Nav.propTypes = {
+   titleText: PropTypes.string.isRequired
+}
 
 export default Nav;
