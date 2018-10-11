964207095102c80b670e61dfe6c1b494891826ad
Sarah Hick <sarah0032@hotmail.com>
2018-10-11 14:37:49 +0200

    Main nav done

---
+++ src/components/nav/nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import Logo from '../../assets/img/OAT_logo.svg';
+
+const Nav = () => (
+   <nav role="navigation" className="main-nav">
+      <Link to="/"><img src={Logo} alt="OAT Home of TAO" className="oat-logo"/></Link>
+   </nav>
+);
+
+export default Nav;
